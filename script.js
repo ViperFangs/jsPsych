daba7870f2aa258163eddabba60892117f5eb6b8
Aarya <amathrej@asu.edu>
2022-12-02 04:06:58 -0700

    Implement a reaction time task

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-const jsPsych = initJsPsych();
+const jsPsych = initJsPsych({
+  on_finish: function() {
+    jsPsych.data.displayData();
+  }
+});
 
 let timeline = [];
 
-const hello_trial = {
+let preload = {
+  type: jsPsychPreload,
+  images: ['img/blue.png', 'img/orange.png']
+};
+
+timeline.push(preload);
+
+const welcome = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: '<p style="font-size: 48px;">Welcome to the Experiment!</p>'
 }
 
-timeline.push(hello_trial);
+timeline.push(welcome);
 
-const hello_trial2 = {
+let instructions = {
   type: jsPsychHtmlKeyboardResponse,
-  stimulus: '<p style="font-size: 24px;">Press \'Space\' or \'Enter\' to proceed forward!</p>', 
-  choices: [' ', "Enter"]
-}
+  stimulus: `
+    <p>In this experiment, a circle will appear in the center 
+    of the screen.</p><p>If the circle is <strong>blue</strong>, 
+    press the letter F on the keyboard as fast as you can.</p>
+    <p>If the circle is <strong>orange</strong>, press the letter J 
+    as fast as you can.</p>
+    <div style='width: 700px;'>
+    <div style='float: left;'><img src='img/blue.png'></img>
+    <p class='small'><strong>Press the F key</strong></p></div>
+    <div style='float: right;'><img src='img/orange.png'></img>
+    <p class='small'><strong>Press the J key</strong></p></div>
+    </div>
+    <p>Press any key to begin.</p>
+  `,
+  post_trial_gap: 2000
+};
 
-timeline.push(hello_trial2);
+timeline.push(instructions);
 
-let judgment_trials = {
+let test_stimuli = [
+  { stimulus: "img/blue.png"},
+  { stimulus: "img/orange.png"}
+];
+
+let fixation = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:60px;">+</div>',
+  choices: "NO_KEYS",
+  trial_duration: function(){
+    return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
+  }
+};
+
+let test = {
   type: jsPsychImageKeyboardResponse,
-  prompt: '<p>Press a number 1-7 to indicate how unusual the image is.</p>',
-  choices: ['1','2','3','4','5','6','7', ' ', "Enter"],
-  timeline: [
-      {stimulus: 'image1.png'},
-      {stimulus: 'image2.png'},
-      {stimulus: 'image3.png'}
-  ]
+  stimulus: jsPsych.timelineVariable('stimulus'),
+  choices: ['f', 'j']
 }
 
-timeline.push(judgment_trials);
+let test_procedure = {
+  timeline: [fixation, test],
+  timeline_variables: test_stimuli,
+  randomize_order: true,
+  repetitions: 5
+}
+
+timeline.push(test_procedure);
 
 jsPsych.run(timeline);
