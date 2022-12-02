aad302f976cd24db6cf1d0778516a0f76226f3b3
Aarya <amathrej@asu.edu>
2022-12-02 01:49:49 -0700

    Add jspsych plugins

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-import {initJsPsych} from 'jspsych';
-import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
-
 const jsPsych = initJsPsych();
 
-const trial = {
-  type: htmlKeyboardResponse,
-  stimulus: 'Hello world!',
+let timeline = [];
+
+const hello_trial = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<p style="font-size: 48px;">Welcome to the Experiment!</p>'
 }
 
-jsPsych.run([trial]);
+timeline.push(hello_trial);
+
+const hello_trial2 = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<p style="font-size: 48px;">Welcome to the Experiment2!</p>'
+}
+
+timeline.push(hello_trial2);
+
+let judgment_trials = {
+  type: jsPsychImageKeyboardResponse,
+  prompt: '<p>Press a number 1-7 to indicate how unusual the image is.</p>',
+  choices: ['1','2','3','4','5','6','7'],
+  timeline: [
+      {stimulus: 'image1.png'},
+      {stimulus: 'image2.png', prompt: '<p>Press 1 for this trial.</p>'},
+      {stimulus: 'image3.png'}
+  ]
+}
+
+timeline.push(judgment_trials);
+
+jsPsych.run(timeline);
